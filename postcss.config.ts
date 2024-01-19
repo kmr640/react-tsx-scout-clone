220c95c5b5589bd51631820db7723dd5f2107106
kmr640 <abckevin.ai@onmail.com>
2024-01-19 12:52:01 +0100

    converted configs from js to ts

---
+++ postcss.config.ts
@@ -0,0 +1,12 @@
+// postcss.config.ts
+
+import autoprefixer from "autoprefixer"
+import tailwindcss from "tailwindcss"
+
+module.exports = {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    // Add other PostCSS plugins here
+  ],
+}
